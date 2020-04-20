d8f497975fe826f2c3c9aa4d92bfdd3d05614f37
ccallejas <Ccallejas@intergrupo.com>
2020-04-20 11:52:54 -0500

    2 click en el test cases para borrar

--- Scripts/borrareste/Script1587400276451.groovy
+++ Scripts/borrareste/Script1587400276451.groovy
@@ -15,3 +15,7 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 
+WebUI.click(findTestObject(null))
+
+WebUI.click(findTestObject(null))
+
